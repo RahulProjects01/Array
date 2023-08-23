19c79a1c492d9d8c72cedfb23f9d244eb19e3baf
Rahul <srahulb6@gmail.com>
2023-08-23 10:25:02 +0530

    message

---
+++ Array.java
@@ -0,0 +1,39 @@
+
+import java.util.*;
+
+public class Array {
+
+    static void printArray(int a[]) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+    }
+
+    static void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    static void sorted(int a[]) {
+        int left = 0;
+        int right = a.length - 1;
+
+        while (left < right) {
+            if (a[left] > a[right]) {
+                swap(a, left, right);
+            }
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int a[] = { 1, 0, 1, 0, 1, 0 };
+        printArray(a);
+        sorted(a);
+        printArray(a);
+    }
+
+}
