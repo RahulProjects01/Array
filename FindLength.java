13f5644d894f861844573a74afa3a856a17f7292
RahulProjects01 <srahulb6@gmail.com>
2023-08-25 13:38:24 +0530

    arry

---
+++ FindLength.java
@@ -0,0 +1,12 @@
+public class FindLength {
+
+    public static void main(String[] args) {
+        int a[] = { 1, 22, 33, 44, 4, 3, 43, 33 };
+        int count = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            count++;
+            System.out.println(count);
+        }
+    }
+}
